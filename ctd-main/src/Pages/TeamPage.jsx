1e69e677afd1bbb066fbfa4c6f7209b8b3a84e31
blueinabaux <bauxblue@gmail.com>
2024-07-27 10:59:31 +0530

    Change UI

--- ctd-main/src/Pages/TeamPage.jsx
+++ ctd-main/src/Pages/TeamPage.jsx
@@ -1,36 +1,64 @@
 import TeamCards from "../components/TeamCards";
+import team1 from "./TeamAssets/team1.png";
+import team2 from "./TeamAssets/team2.png";
+import team3 from "./TeamAssets/team3.png";
 const TeamPage = () => {
 
     const teamArr = [
         {
             id:1,
             name: "SYKUNNO",
-            role:"Backend Developer"
+            role:"Backend Developer",
+            image: team1
         },
         {
             id:2,
             name: "NEEL",
-            role:"Frontend Developer"
+            role:"Frontend Developer",
+            image: team2
+
         },
         {
             id:3,
             name: "MILI",
-            role:"Designer"
+            role:"Designer",
+            image: team3
+
+        },
+        {
+            id:4,
+            name: "SYKUNNO",
+            role:"Backend Developer",
+            image: team1
+        },
+        {
+            id:5,
+            name: "NEEL",
+            role:"Frontend Developer",
+            image: team2
+
+        },
+        {
+            id:6,
+            name: "MILI",
+            role:"Designer",
+            image: team3
+
         },
     ]
     return ( 
         <>
         <div className="team-cont min-h-[100vh] w-[100vw] bg-[#181818] flex flex-col justify-start items-center ">
-            <div className="events-head h-[30vh] w-[100vw] bg--600 flex justify-start items-center px-[2vw] ">
+            <div className="events-head h-[30vh] w-[100vw] bg--600 flex justify-start items-end px-[5vw]  ">
                 <h1 className="text-white text-[6vw]">OUR TEAM</h1>
             </div>
 
-            <div className="events-head h-[100vh] w-[100vw] bg--600 flex flex-wrap justify-around items-start py-[10vh]  ">
+            <div className="events-head h-[100vh] w-[100vw] bg--600 flex flex-wrap justify-center items-start py-[10vh]  ">
                 {
                     teamArr.map((team) => {
                         return(
                             <>
-                                <TeamCards key={team.id} name={team.name} role={team.role} />
+                                <TeamCards key={team.id} name={team.name} role={team.role} image={team.image} />
                             </>
                         );
                     })
--- ctd-main/src/components/TeamCards.jsx
+++ ctd-main/src/components/TeamCards.jsx
@@ -1,19 +1,19 @@
 
 import { FaArrowRight } from "react-icons/fa6";
 
-const TeamCards = ({name, role}) => {
+const TeamCards = ({name, role, image}) => {
     return ( 
         <>
-            <div className="team-card h-[53vh] w-[22vw] bg--400 flex flex-col justify-center items-center rounded-[2vh]">
-                <img src="" alt="" />
-                <div className="team-info h-[100%] w-[100%] bg-black flex justify-center items-end rounded-[2vh]">
+            <div className="team-card h-[53vh] w-[20vw] bg--400 flex flex-col justify-center items-center rounded-[20px] relative">
+                <img src={image} alt="IMAGE" className="h-full w-full object-cover rounded-[20px]  absolute top-0 left-0 z-[1000] text-white" />
+                <div className="team-info h-[100%] w-[100%] bg-transparent absolute top-0 left-0 flex justify-center items-end rounded-[20px] opacity-[100%] z-[10000] ">
                     <div className="team-bottom h-[16vh] w-[100%] bg--500 flex justify-center items-center">
-                        <div className="team-left h-[100%] w-[60%] bg--500 flex flex-col justify-center items-start px-[2vw]">
+                        <div className="team-left h-[100%] w-[60%] bg--500 flex flex-col justify-center items-start px-[1.5vw]">
                             <h1 className="text-white text-[6vh] leading-[8vh]">{name}</h1>
-                            <p className="text-white text-[1.8vh]">{role}</p>
+                            <p className="text-white text-[2vh] bg--500">{role}</p>
                         </div>
                         <div className="team-right h-[100%] w-[40%] bg--500 flex  justify-center items-center">
-                            <button className="bg-white rounded-full h-[10vh] w-[10vh] flex  justify-center items-center"><FaArrowRight className="z-[3000]" /></button>
+                            <button className="bg-white rounded-full h-[10vh] w-[10vh] flex  justify-center items-center"><FaArrowRight className="z-[2000] rotate-[-45deg] " /></button>
                         </div>
                     </div>
                 </div>
