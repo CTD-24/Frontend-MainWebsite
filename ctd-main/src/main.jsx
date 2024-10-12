f95dfc78be998fb8ee5ac160dbcfc3dcc0a37298
nk112233 <nikunjkadu19@gmail.com>
2024-10-12 19:28:26 +0530

    Minor Fixes

--- ctd-main/src/main.jsx
+++ ctd-main/src/main.jsx
@@ -85,7 +85,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <ToastContainer
 position="top-center"
 autoClose={2000}
-hideProgressBar={true}
+hideProgressBar={false}
 newestOnTop={false}
 closeOnClick
 rtl={false}
