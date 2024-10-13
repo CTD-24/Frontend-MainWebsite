32e12622716b7a57eb4d014497e90bc00ee0ba71
nk112233 <nikunjkadu19@gmail.com>
2024-10-13 13:26:48 +0530

    Proxy Fix

--- ctd-main/vite.config.js
+++ ctd-main/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
   // Load environment variables based on the current mode
@@ -11,18 +11,9 @@ export default defineConfig(({ mode }) => {
       'import.meta.env': {
         VITE_TEAM_EXIST_URL: JSON.stringify(env.VITE_TEAM_EXIST_URL),
         VITE_REG_URL: JSON.stringify(env.VITE_REG_URL),
-        VITE_UTR_EXIST_URL: JSON.stringify(env.VITE_UTR_EXIST_URL),
-      },
+        VITE_UTR_EXIST_URL: JSON.stringify(env.VITE_UTR_EXIST_URL)
+      }
     },
     plugins: [react()],
-    server: {
-      proxy: {
-        '/api': {
-          target: 'https://finalbackend.api.ctd.credenz.co.in', // Your actual backend URL
-          changeOrigin: true,
-          // No need to rewrite the path since the target already includes /api
-        },
-      },
-    },
-  };
+  }
 });
